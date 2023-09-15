b7992e8b23a02c21d83c9eb8bcfcab5ee886fef9
MrAtrex <145151655+MrAtrex@users.noreply.github.com>
2023-09-15 21:39:02 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b;
+        char c, again;
+        Console.WriteLine("Введите a ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите способ расчёта (+, -, *, /)");
+        c = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("Введите b ");
+        b = Convert.ToDouble(Console.ReadLine());
+        switch (c)
+        {
+            case '+':
+                Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+                break;
+            case '-':
+                Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+                break;
+            case '*':
+                Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+                break;
+            case '/':
+                Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+                break;
+            default:
+                Console.WriteLine("Ошибка");
+                break;
+            }
+    }
+}
