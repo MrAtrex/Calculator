cbead8263bed2f5141cd81e1e6138b5b7c764024
Anastasia <girl-physicist@mail.ru>
2023-09-26 17:12:30 +0300

    recomend

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,16 +1,16 @@
 ﻿internal class Program
 {
+        double a, b;// следует давать значимые (передающие смысл) имена. Например number1 или firstNum
+        char c; // следует давать значимые (передающие смысл) имена. Например typeOfTransaction
+     // в методе Main не должно быть расчетов, весь функционал выносится в отдельные методы, а тут только их вызывают
     private static void Main(string[] args)
     {
-        double a, b;
-        char c;
-        Console.WriteLine("Введите a ");
-        a = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Введите способ расчёта (+, -, *, /)");
-        c = Convert.ToChar(Console.ReadLine());
-        Console.WriteLine("Введите b ");
-        b = Convert.ToDouble(Console.ReadLine());
-        switch (c)
+       GetInputData();
+       Calc();        
+    }
+     static void Calc()
+     {
+       switch (c)
         {
             case '+':
                 Console.WriteLine("{0}+{1}={2}", a, b, a + b);
@@ -27,6 +27,15 @@
             default:
                 Console.WriteLine("Ошибка");
                 break;
-        }
-    }
+        }  
+     }
+     static void GetInputData()
+     {
+         Console.WriteLine("Введите a ");
+        a = Convert.ToDouble(Console.ReadLine()); // следовало добавить обработку исключения на случай, если не удалось конвертировать строку в число (например ввели дробное число или буквы)
+        Console.WriteLine("Введите способ расчёта (+, -, *, /)");
+        c = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("Введите b ");
+        b = Convert.ToDouble(Console.ReadLine());         
+     }
 }
